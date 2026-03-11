d1e0de7388ebc0a2bd294ca917b37d007cae037a
Omar Fekih <omarfh788@gmail.com>
2026-03-11 13:08:52 +0100

    chore: remove gradio stub from webapp

--- webapp/app.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-with gr.Blocks() as demo:
-    gr.Markdown("# Wholesale Customer Segmentation")
-    gr.Markdown("WebApp en cours de développement...")
-
-if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
